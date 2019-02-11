92bd2d42497ffec25b01c3973451e14c1edd0890
Michal Kimle <kimle.michal@gmail.com>
2019-02-11 15:31:00 +0100

    Configure and run service on Cloudkeeper-OS start

--- cloudkeeper_os/main.py
+++ cloudkeeper_os/main.py
@@ -1,9 +1,13 @@
 """
 Main entry point for Cloudkeeper-OS
 """
+from cloudkeeper_os import configuration
+from cloudkeeper_os.grpc import server
 
 
 def run():
     """
     Main method run for Cloudkeeper-OS
     """
+    configuration.configure()
+    server.serve()
